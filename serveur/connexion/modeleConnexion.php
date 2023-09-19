f0fde994259ee58e2e3fd208b51a918971276490
walezkamarrder <116007333+walezkamarrder@users.noreply.github.com>
2023-09-19 10:46:47 -0400

    session_start

--- serveur/connexion/modeleConnexion.php
+++ serveur/connexion/modeleConnexion.php
@@ -14,13 +14,27 @@
             if ($reponse->num_rows > 0) { // OK, courriel et mot de passe existent
                 $ligne = $reponse->fetch_object();
                 if($ligne->statut == 'A'){
+                    //obtenir les infos du membre
+                    $requete = "SELECT * FROM connexion WHERE courriel=?";
+                    $stmt = $connexion->prepare($requete);
+                    $stmt->bind_param("s", $courriel);
+                    $stmt->execute();
+                    $reponse = $stmt->get_result();
+                    $ligne = $reponse -> fetch_objet();
                     if($ligne->role == 'M'){
+                        $_SESSION['role'] = 'M';
+                        $_SESSION['prenom'] = $ligne->prenom;
+                        $_SESSION['nom'] = $ligne->nom;
                         header('Location: ../membre/membre.php');
                         exit();
                     } else { // Dans ce cas c'est un admin
+                        $_SESSION['role']= 'A';
+                        $_SESSION['prenom'] = $ligne->prenom;
+                        $_SESSION['nom'] = $ligne->nom;
                         header('Location: ../admin/admin.php');
                         exit();
                     }
+                    
                 } else {// Membre inactif
                     $msg = "<b>SVP contactez l'administrateur !!!</b>";
                 }
--- serveur/membre/membre.php
+++ serveur/membre/membre.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    if(!isset( $_SESSION['role'])){
+        header('location: ../../index.php');
+    }
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
